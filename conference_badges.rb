c7c33134d03e8bbc2b58c8e9aa0f414276792d40
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-15 03:06:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator(names)
   names.map{ |name| badge_maker(name) }
 end
 
-def assign_rooms
+def assign_rooms(speaker)
   counter = 1 
   until counter == 7
   speaker each.do |speaker|
